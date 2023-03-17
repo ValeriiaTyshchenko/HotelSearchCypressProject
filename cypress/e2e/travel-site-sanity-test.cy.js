779ab55af175aac97bdf33887c9a975e3477216d
Valeriia Tyshchenko <valeriia.test.tys@gmail.com>
2023-03-17 11:30:57 -0400

    first commit

---
+++ cypress/e2e/travel-site-sanity-test.cy.js
@@ -0,0 +1,30 @@
+///<reference types = "Cypress"/>
+describe('The sanity test suit for travel site', () => {
+    it('navigate to main page', () => {
+      cy.visit('http://localhost:3000')
+      cy.url().should('eq', 'http://localhost:3000/')
+    })
+
+    it('verify tabs of the page', () => {
+        cy.visit('http://localhost:3000')
+        cy.get('[data-cy="Hotels"]').should("be.visible")
+        cy.get('[data-cy="Car"]').should("be.visible")
+        cy.get('[data-cy="Flight"]').should("be.visible")
+      })
+
+      it('verify text of tabs of the page', () => {
+        cy.visit('http://localhost:3000')
+        cy.get('[data-cy="Hotels"]').should("have.text", "hotels")
+        cy.get('[data-cy="Car"]').should("have.text", "car")
+        cy.get('[data-cy="Flight"]').should("have.text", "flight")
+      })
+
+      it('verify elements of Hotels tab', () => {
+        cy.visit('http://localhost:3000')
+        cy.get('[data-cy="flight_chk"]').should("be.visible")
+        cy.get('[data-cy="cars_chk"]').should("be.visible")
+        // cy.get('#tab1 > form > .btn-submit').should("be.visible")
+        // cy.contains('search').should("be.visible")
+        cy.get('[type="submit"]').should("be.visible")
+      })
+  })
